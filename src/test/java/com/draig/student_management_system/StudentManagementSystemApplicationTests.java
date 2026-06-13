0033084f268a12a909032e4947d852812632f016
grv <garv.sharma200212@gmail.com>
2026-06-13 18:05:29 +0530

    Initial commit

---
+++ src/test/java/com/draig/student_management_system/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.draig.student_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
