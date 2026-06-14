f3936853a03a8e9362a0989b5eedb4ee76a29dac
grv <garv.sharma200212@gmail.com>
2026-06-14 23:41:21 +0530

    defined the basic structure of Student

---
+++ src/main/java/com/draig/student_management_system/entity/Student.java
@@ -0,0 +1,22 @@
+package com.draig.student_management_system.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
