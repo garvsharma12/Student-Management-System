43113a8967c5783d2e863ab0e76ecdf8fa4a36d1
grv <garv.sharma200212@gmail.com>
2026-06-18 21:40:03 +0530

    created getStudent API

--- src/main/java/com/draig/student_management_system/controller/StudentController.java
+++ src/main/java/com/draig/student_management_system/controller/StudentController.java
@@ -1,8 +1,26 @@
 package com.draig.student_management_system.controller;
 
+import com.draig.student_management_system.dto.StudentDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.draig.student_management_system.service.StudentService;
+
+import java.util.List;
 
 @Controller
 public class StudentController {
+    private StudentService studentService;
 
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    //handler methods to handle list students request
+    @GetMapping("/students")
+    public String listStudents(Model model){
+        List<StudentDto> students = studentService.getAllStudents();
+        model.addAttribute("students", students);
+        return "students";
+    }
 }
--- src/main/java/com/draig/student_management_system/service/impl/StudentServiceImpl.java
+++ src/main/java/com/draig/student_management_system/service/impl/StudentServiceImpl.java
@@ -1,11 +1,15 @@
 package com.draig.student_management_system.service.impl;
 
 import com.draig.student_management_system.dto.StudentDto;
+import com.draig.student_management_system.entity.Student;
+import com.draig.student_management_system.mapper.StudentMapper;
 import com.draig.student_management_system.repository.StudentRepository;
 import com.draig.student_management_system.service.StudentService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class StudentServiceImpl implements StudentService {
@@ -17,6 +21,10 @@ public class StudentServiceImpl implements StudentService {
     }
     @Override
     public List<StudentDto> getAllStudents() {
-        return List.of();
+        List<Student> students = studentRepository.findAll();
+        List<StudentDto> studentDtos = students.stream()
+                .map(StudentMapper::mapToStudentDto)
+                .collect(Collectors.toList());
+        return studentDtos;
     }
 }
